7d9d46ff9bb551436ad3ec4b8128b16bfd4ed49f
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-05-25 13:21:23 +0300

    HW5 completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -27,13 +27,13 @@ func Run(tasks []Task, n, m int) error {
 	for iTask := 0; iTask < len(tasks); iTask++ {
 		fmt.Printf("start iTask=%d\n", iTask)
 		for i := 0; i < n; i++ {
-			// fmt.Printf("start in gorutine n=%d iTask=%d\n", i, iTask)
+			// fmt.Printf("start in gorutine n=%d iTask=%d\n", i, iTask).
 			wg.Add(1)
-			// fmt.Printf("ready to run iTask=%d,i=%d\n", iTask, i)
+			// fmt.Printf("ready to run iTask=%d,i=%d\n", iTask, i).
 			go func(taskNum int) {
 				if taskNum < len(tasks) {
 					err := tasks[taskNum]()
-					// fmt.Printf("ran iTask=%d\n", taskNum)
+					// fmt.Printf("ran iTask=%d\n", taskNum).
 					if err != nil {
 						mu.Lock()
 						errCnt++
@@ -48,11 +48,11 @@ func Run(tasks []Task, n, m int) error {
 		}
 		wg.Wait()
 		if errCnt >= m {
-			// fmt.Printf("ErrErrorsLimitExceeded in end iTask=%d\n", iTask)
+			// fmt.Printf("ErrErrorsLimitExceeded in end iTask=%d\n", iTask).
 			return ErrErrorsLimitExceeded
 		}
 		iTask--
 	}
-	// fmt.Printf("received errCnt=%d errors limit=%d\n", errCnt, m)
+	// fmt.Printf("received errCnt=%d errors limit=%d\n", errCnt, m).
 	return nil
 }
