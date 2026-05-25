7044b89e6752f6d7572cf9fe5a292405d8601c0d
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-05-25 13:14:14 +0300

    HW5 completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,47 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	errCnt := 0
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	// игнорировать ошибки в принципе
+	if m < 0 {
+		m = len(tasks) * 100
+	}
+
+	if n > len(tasks) {
+		n = len(tasks)
+	}
+
+	for iTask := 0; iTask < len(tasks); iTask++ {
+		fmt.Printf("start iTask=%d\n", iTask)
+		for i := 0; i < n; i++ {
+			// fmt.Printf("start in gorutine n=%d iTask=%d\n", i, iTask)
+			wg.Add(1)
+			// fmt.Printf("ready to run iTask=%d,i=%d\n", iTask, i)
+			go func(taskNum int) {
+				if taskNum < len(tasks) {
+					err := tasks[taskNum]()
+					// fmt.Printf("ran iTask=%d\n", taskNum)
+					if err != nil {
+						mu.Lock()
+						errCnt++
+						mu.Unlock()
+					}
+				}
+				wg.Done()
+			}(iTask)
+			mu.Lock()
+			iTask++
+			mu.Unlock()
+		}
+		wg.Wait()
+		if errCnt >= m {
+			// fmt.Printf("ErrErrorsLimitExceeded in end iTask=%d\n", iTask)
+			return ErrErrorsLimitExceeded
+		}
+		iTask--
+	}
+	// fmt.Printf("recieved errCnt=%d errors limit=%d\n", errCnt, m)
 	return nil
 }
