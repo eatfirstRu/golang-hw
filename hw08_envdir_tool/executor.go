1abac936317241ff31a97de1d802376eb998dd2d
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-06-04 15:45:16 +0300

    HW8 completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for k, v := range env {
+		if v.NeedRemove {
+			err := os.Unsetenv(k)
+			if err != nil {
+				return 100
+			}
+		} else {
+			os.Setenv(k, v.Value)
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stderr = os.Stderr
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+
+	err := command.Run()
+	if err != nil {
+		// fmt.Printf("command.Run() has error: %T, %v", err, err).
+		return 101
+	}
+
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	env, err := ReadDir(os.Args[0])
+	if err != nil {
+		fmt.Printf("error during read dir: %v", err)
+		os.Exit(103)
+		return
+	}
+
+	if len(os.Args) < 4 {
+		fmt.Printf("uncorrect count args\n")
+		os.Exit(104)
+		return
+	}
+
+	rslt := RunCmd(os.Args[1:], env)
+	os.Exit(rslt)
 }
