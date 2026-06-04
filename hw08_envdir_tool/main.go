21313cf4d280327b91f31c9bdffc6172fb71c1e0
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-06-04 15:52:55 +0300

    HW8 completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	env, err := ReadDir(os.Args[0])
+	env, err := ReadDir(os.Args[1])
 	if err != nil {
 		fmt.Printf("error during read dir: %v", err)
 		os.Exit(103)
 		return
 	}
 
-	if len(os.Args) < 4 {
+	if len(os.Args) < 5 {
 		fmt.Printf("uncorrect count args\n")
 		os.Exit(104)
 		return
 	}
 
-	rslt := RunCmd(os.Args[1:], env)
+	rslt := RunCmd(os.Args[2:], env)
 	os.Exit(rslt)
 }
