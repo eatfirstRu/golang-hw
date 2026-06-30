1005e15da5ee326f62f98c4fc8bcc9d69d4f2204
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-06-30 13:25:32 +0300

    HW 9 completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,48 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+var (
+	// ErrZeroValue is the error returned when variable has zero value
+	// and nonzero or nonnil was specified
+	ErrZeroValue = errors.New("zero value")
+	// ErrMin is the error returned when variable is less than mininum
+	// value specified
+	ErrMin = errors.New("less than min")
+	// ErrMax is the error returned when variable is more than
+	// maximum specified
+	ErrMax = errors.New("greater than max")
+	// ErrLen is the error returned when length is not equal to
+	// param specified
+	ErrLen = errors.New("invalid length")
+	// ErrRegexp is the error returned when the value does not
+	// match the provided regular expression parameter
+	ErrRegexp = errors.New("regular expression mismatch")
+	ErrIn     = errors.New("values not in enumeration")
+	// ErrUnsupported is the error error returned when a validation rule
+	// is used with an unsupported variable type
+	ErrUnsupported = errors.New("unsupported type")
+	// ErrBadParameter is the error returned when an invalid parameter
+	// is provided to a validation rule (e.g. a string where an int was
+	// expected (max=foo,len=bar) or missing a parameter when one is required (len=))
+	ErrBadParameter = errors.New("bad parameter")
+	// ErrUnknownTag is the error returned when an unknown tag is found
+	ErrUnknownTag = errors.New("unknown tag")
+	// ErrInvalid is the error returned when variable is invalid
+	// (normally a nil pointer)
+	ErrInvalid = errors.New("invalid value")
+	// ErrCannotValidate is the error returned when a struct is unexported
+	ErrCannotValidate = errors.New("cannot validate unexported struct")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +51,132 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var err error
+	if len(ve) == 0 {
+		return ""
+	}
+	if len(v) == 1 {
+		return fmt.Errorf("field: %s %w ", v[0].Field, v[0].Err).Error()
+	}
+
+	for _, ve := range v {
+		err = errors.Join(fmt.Errorf("field: %s %w ", ve.Field, ve.Err), err)
+	}
+	return err.Error()
 }
 
+var ve = make(ValidationErrors, 0)
+
 func Validate(v interface{}) error {
-	// Place your code here.
-	return nil
+	funcMap := map[string]struct{}{
+		"len":    {},
+		"min":    {},
+		"max":    {},
+		"in":     {},
+		"regexp": {},
+	}
+	st := reflect.TypeOf(v)
+	clear(ve)
+
+	for i := 0; i < st.NumField(); i++ {
+		field := st.Field(i)
+		tag := field.Tag.Get("validate")
+		val := reflect.ValueOf(v).FieldByName(field.Name) //.Interface()
+		if tag != "" {
+			vldt := make([]map[string]string, 0)
+			andCond := strings.Split(tag, "|")
+			for _, pair := range andCond {
+				mp := make(map[string]string)
+				parts := strings.Split(pair, ":")
+				if len(parts) != 2 {
+					// err := fmt.Errorf("%w: %s", ErrUnknownTag, parts)
+					ve = append(ve, ValidationError{Field: field.Name, Err: ErrUnknownTag}) // err})
+					continue
+				}
+				mp[parts[0]] = parts[1]
+				vldt = append(vldt, mp)
+				if _, ok := funcMap[parts[0]]; ok {
+					checkParam(field.Name, parts[0], parts[1], val)
+				} else {
+					continue
+				}
+			}
+		}
+	}
+	if len(ve) == 0 || ve[0].Err == nil {
+		return nil
+	}
+	return ve
+}
+
+func appendVE(fieldName string, err error) {
+	if len(ve) != 0 && ve[0].Err == nil {
+		ve[0] = ValidationError{Field: fieldName, Err: err}
+	} else {
+		ve = append(ve, ValidationError{Field: fieldName, Err: err})
+	}
+}
+
+func convToInt(paramVal, fieldName string) (int, error) {
+	pv, err := strconv.Atoi(paramVal)
+	if err != nil {
+		appendVE(fieldName, ErrBadParameter)
+		return 0, ErrBadParameter
+	}
+	return pv, nil
+}
+
+func checkParam(fieldName, paramName, paramVal string, v reflect.Value) {
+	var valid bool
+	var retErr error
+	var pv int
+
+	switch {
+	case v.Kind() == reflect.Slice:
+		for i := 0; i < v.Len(); i++ {
+			checkParam(fieldName, paramName, paramVal, v.Index(i))
+		}
+		valid = true
+	case (paramName == "len" && v.Kind() == reflect.String):
+		if pv, retErr = convToInt(paramVal, fieldName); retErr == nil {
+			valid = utf8.RuneCountInString(v.String()) == pv
+			retErr = ErrLen
+		}
+	case (paramName == "min" && v.Kind() == reflect.Int):
+		if pv, retErr = convToInt(paramVal, fieldName); retErr == nil {
+			valid = v.Int() > int64(pv)
+			retErr = ErrMin
+		}
+	case (paramName == "max" && v.Kind() == reflect.Int):
+		if pv, retErr = convToInt(paramVal, fieldName); retErr == nil {
+			valid = v.Int() < int64(pv)
+			retErr = ErrMax
+		}
+	case (paramName == "in" && (v.Kind() == reflect.Int || v.Kind() == reflect.String)):
+		sl := strings.Split(paramVal, ",")
+		retErr = ErrIn
+
+		for _, vSl := range sl {
+			if v.Kind() == reflect.Int {
+				if pv, err := convToInt(vSl, fieldName); err == nil {
+					valid = v.Int() == int64(pv)
+				}
+			}
+			if v.Kind() == reflect.String {
+				valid = v.String() == vSl
+			}
+			if valid {
+				break
+			}
+		}
+	case paramName == "regexp":
+		retErr = ErrRegexp
+		re := *regexp.MustCompile(paramVal)
+		valid = re.MatchString(v.String())
+	default:
+		appendVE(fieldName, ErrUnsupported)
+	}
+	if !valid && retErr != ErrBadParameter {
+		appendVE(fieldName, retErr)
+	}
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -20,8 +23,30 @@ type (
 		meta   json.RawMessage //nolint:unused
 	}
 
+	UserAge struct {
+		Age int `validate:"min:18|max:50"`
+	}
+	Role struct {
+		Role UserRole `validate:"in:admin,stuff"`
+	}
+
 	App struct {
 		Version string `validate:"len:5"`
+	}
+
+	AppInvalidTag struct {
+		Version string `validate:"len5"`
+	}
+	AppInvalidTag2 struct {
+		Version string `validate:"len:5y"`
+	}
+
+	Nums struct {
+		Version string `validate:"regexp:\\d+|len:20"`
+	}
+
+	UserEmail struct {
+		Email string `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
 	}
 
 	Token struct {
@@ -36,25 +61,84 @@ type (
 	}
 )
 
-func TestValidate(t *testing.T) {
-	tests := []struct {
-		in          interface{}
-		expectedErr error
-	}{
-		{
-			// Place your code here.
-		},
-		// ...
-		// Place your code here.
-	}
+func makeVe(field string, err error) ValidationErrors {
+	ve := make(ValidationErrors, 1)
+	ve[0] = ValidationError{field, err}
+	return ve
+}
 
+var tests = []struct {
+	in          interface{}
+	expectedErr error
+}{
+	{
+		in:          AppInvalidTag{Version: "1234"},
+		expectedErr: makeVe("Version", ErrUnknownTag),
+	},
+	{
+		in:          AppInvalidTag2{Version: "1234"},
+		expectedErr: makeVe("Version", ErrBadParameter),
+	},
+	{
+		in:          App{Version: "12345"},
+		expectedErr: nil,
+	},
+
+	{
+		in:          App{Version: "1234"},
+		expectedErr: makeVe("Version", ErrLen),
+	},
+	{
+		in:          UserAge{Age: 10},
+		expectedErr: makeVe("Age", ErrMin),
+	},
+
+	{
+		in:          UserAge{Age: 100},
+		expectedErr: makeVe("Age", ErrMax),
+	},
+
+	{
+		in:          Response{Code: 100},
+		expectedErr: makeVe("Code", ErrIn),
+	},
+	{
+		in:          Response{Code: 200},
+		expectedErr: nil,
+	},
+	{
+		in:          Role{Role: "none"},
+		expectedErr: makeVe("Role", ErrIn),
+	},
+	{
+		in:          Role{Role: "admin"},
+		expectedErr: nil,
+	},
+	{
+		in:          UserEmail{Email: "ggg*mail.su"},
+		expectedErr: makeVe("Email", ErrRegexp),
+	},
+	{
+		in:          UserEmail{Email: "ggg@mail.su"},
+		expectedErr: nil,
+	},
+}
+
+func TestValidate(t *testing.T) {
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
-			t.Parallel()
+			/*tt := tt
+			t.Parallel()*/
+			err := Validate(tt.in)
 
-			// Place your code here.
-			_ = tt
+			if errors.Is(err, tt.expectedErr) {
+				err = tt.expectedErr
+			}
+			if tt.expectedErr == nil {
+				require.NoError(t, err, tt.expectedErr)
+			} else {
+				require.EqualError(t, err, tt.expectedErr.Error())
+			}
 		})
 	}
 }
