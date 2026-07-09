19eb9d13e75e3746acc85b895317a20e2ab4b33a
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-07-09 14:37:51 +0300

    HW12 completed

--- hw12_13_14_15_16_calendar/internal/logger/logger.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger.go
@@ -1,46 +1,77 @@
 package logger
 
 import (
-	"log/slog"
+	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
+const (
+	levelDebug = iota
+	levelInfo
+	levelWarn
+	levelError
+)
+
 type Logger struct {
-	logger *slog.Logger
+	level  int
+	logger *log.Logger
 }
 
 func New(level string) *Logger {
-	var lvl slog.Level
+	var lvl int
 	switch strings.ToLower(level) {
 	case "debug":
-		lvl = slog.LevelDebug
+		lvl = levelDebug
 	case "info":
-		lvl = slog.LevelInfo
+		lvl = levelInfo
 	case "warn", "warning":
-		lvl = slog.LevelWarn
+		lvl = levelWarn
 	case "error":
-		lvl = slog.LevelError
+		lvl = levelError
 	default:
-		lvl = slog.LevelInfo
+		lvl = levelInfo
 	}
 
-	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: lvl})
-	return &Logger{logger: slog.New(handler)}
+	return &Logger{
+		level:  lvl,
+		logger: log.New(os.Stdout, "", log.LstdFlags),
+	}
 }
 
-func (l *Logger) Debug(msg string, args ...any) {
-	l.logger.Debug(msg, args...)
+func (l *Logger) Debug(msg string, args ...interface{}) {
+	if l.level <= levelDebug {
+		l.log("DEBUG", msg, args...)
+	}
 }
 
-func (l *Logger) Info(msg string, args ...any) {
-	l.logger.Info(msg, args...)
+func (l *Logger) Info(msg string, args ...interface{}) {
+	if l.level <= levelInfo {
+		l.log("INFO", msg, args...)
+	}
 }
 
-func (l *Logger) Warn(msg string, args ...any) {
-	l.logger.Warn(msg, args...)
+func (l *Logger) Warn(msg string, args ...interface{}) {
+	if l.level <= levelWarn {
+		l.log("WARN", msg, args...)
+	}
 }
 
-func (l *Logger) Error(msg string, args ...any) {
-	l.logger.Error(msg, args...)
+func (l *Logger) Error(msg string, args ...interface{}) {
+	if l.level <= levelError {
+		l.log("ERROR", msg, args...)
+	}
+}
+
+func (l *Logger) log(level, msg string, args ...interface{}) {
+	if len(args) > 0 {
+		kvs := make([]string, 0, len(args)/2)
+		for i := 0; i+1 < len(args); i += 2 {
+			kvs = append(kvs, fmt.Sprintf("%v=%v", args[i], args[i+1]))
+		}
+		l.logger.Printf("[%s] %s %s", level, msg, strings.Join(kvs, " "))
+	} else {
+		l.logger.Printf("[%s] %s", level, msg)
+	}
 }
