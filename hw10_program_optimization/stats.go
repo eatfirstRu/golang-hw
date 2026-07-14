147420a20d68c7b06e8562dccd3c11868a0a2dac
DIASOFT\etroshin <etroshin@diasoft.ru>
2026-07-14 17:05:23 +0300

    HW10 Completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,13 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
+
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
 )
 
 type User struct {
@@ -18,49 +20,58 @@ type User struct {
 	Address  string
 }
 
+func (u *User) UnmarshalEasyJSON(in *jlexer.Lexer) {
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if key == "Email" {
+			u.Email = in.String()
+		} else {
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+}
+
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	suffix := "." + domain
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
+	scanner := bufio.NewScanner(r)
+
+	var user User
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if len(line) == 0 {
+			continue
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		user.Email = ""
+		if err := easyjson.Unmarshal(line, &user); err != nil {
+			return nil, fmt.Errorf("unmarshal error: %w", err)
+		}
+
+		if user.Email == "" {
+			continue
+		}
+
+		if strings.HasSuffix(user.Email, suffix) {
+			idx := strings.IndexByte(user.Email, '@')
+			if idx < 0 {
+				continue
+			}
+			domainPart := strings.ToLower(user.Email[idx+1:])
+			result[domainPart]++
 		}
 	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
 	return result, nil
 }
